c6815536f4847712f0dd3cf4412fd08e2a5e0d4a
Guillaume <Guillaume@192.168.1.133>
2014-09-24 12:42:11 -0400

    Debut Projet

---
+++ src/projetsession/Categorie.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projetsession;
+
+/**
+ *
+ * @author gcoll_000
+ */
+public enum Categorie {
+   
+cours,atelier,seminaire,colloque,conférence,lecture,dirigée, présentation, groupe_de_discussion, projet_de_recherche,rédaction_professionnelle
+    
+}
